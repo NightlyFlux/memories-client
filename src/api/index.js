98dc339f2da9e1180aa439f1368e8c67661a78c7
NightlyFlux <nightlyflux@gmail.com>
2020-12-19 13:19:33 -0800

    Typo

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'https://memories-rest-api.herokuapp.com/posts',
+  baseURL: 'https://memories-rest-api.herokuapp.com',
 })
 
 export default http
